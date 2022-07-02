35ab313de53326ddce24e3789f7919a2cb6476d1
Kent Louise Tonino <69438999+kenttonino@users.noreply.github.com>
2022-07-03 03:23:33 +0800

    Add a Head Icon (#5)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,16 @@
+import Head from "next/head"
+
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Calculator</title>
+        <link rel='icon' href='/head.ico'></link>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        />
+      </Head>
       <button>Record Video</button>
     </>
   )
