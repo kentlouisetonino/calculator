f2f52d8a2c03322caec1051bbc1e873fb83f3626
Kent Louise Tonino <69438999+kenttonino@users.noreply.github.com>
2022-07-03 16:58:37 +0800

    Create Initial Design (#6)

---
+++ src/components/button/types/ButtonProps.ts
@@ -0,0 +1,11 @@
+import ButtonType from './ButtonType'
+
+type Props = {
+  label: string
+  position?: [x: number, y: number]
+  width?: number
+  height?: number
+  type?: ButtonType
+}
+
+export default Props
